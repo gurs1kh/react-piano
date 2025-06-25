bdea7b1ba7b8a03a9cfce482b9f4edf65ddb783d
Manvir Singh <gurs1kh@live.com>
2025-06-24 21:46:22 -0700

    update vite to export types

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,19 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
 import react from '@vitejs/plugin-react'
+import dts from "vite-plugin-dts";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({ tsconfigPath: './tsconfig.app.json' })
+  ],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.js'),
-      formats: ['es']
+      formats: ['es'],
+      fileName: 'index',
     },
   }
 })
