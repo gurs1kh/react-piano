3a639564c9afe41d3abee4433591c7fe5a36d61b
Manvir Singh <gurs1kh@live.com>
2025-06-30 19:43:21 -0700

    convert InteractiveDemo into functional component

--- demo/src/InteractiveDemo.tsx
+++ demo/src/InteractiveDemo.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useState } from 'react';
 import { Piano, KeyboardShortcuts, MidiNumbers } from 'react-piano';
 import { MdArrowDownward } from 'react-icons/md';
 
@@ -13,77 +13,71 @@ interface InteractiveDemoProps {
   soundfontHostname: string;
 }
 
-class InteractiveDemo extends React.Component<InteractiveDemoProps> {
-  state  = {
-    config: {
-      instrumentName: 'acoustic_grand_piano' as InstrumentName,
-      noteRange: {
-        first: MidiNumbers.fromNote('c3'),
-        last: MidiNumbers.fromNote('f5'),
-      },
-      keyboardShortcutOffset: 0,
-    },
-  };
+const InteractiveDemo = ({ audioContext, soundfontHostname }: InteractiveDemoProps) => {
+  const [instrumentName, setInstrumentName] = useState<InstrumentName>('acoustic_grand_piano');
+  const [noteRange, setNoteRange] = useState({
+    first: MidiNumbers.fromNote('c3'),
+    last: MidiNumbers.fromNote('f5'),
+  });
+  const [keyboardShortcutOffset, setKeyboardShortcutOffset] = useState(0);
 
-  render() {
-    const keyboardShortcuts = KeyboardShortcuts.create({
-      firstNote: this.state.config.noteRange.first + this.state.config.keyboardShortcutOffset,
-      lastNote: this.state.config.noteRange.last + this.state.config.keyboardShortcutOffset,
-      keyboardConfig: KeyboardShortcuts.HOME_ROW,
-    });
+  const keyboardShortcuts = KeyboardShortcuts.create({
+    firstNote: noteRange.first + keyboardShortcutOffset,
+    lastNote: noteRange.last + keyboardShortcutOffset,
+    keyboardConfig: KeyboardShortcuts.HOME_ROW,
+  });
 
-    return (
-      <SoundfontProvider
-        audioContext={this.props.audioContext}
-        instrumentName={this.state.config.instrumentName}
-        hostname={this.props.soundfontHostname}
-        render={({ isLoading, playNote, stopNote, stopAllNotes }) => (
-          <div>
-            <div className="text-center">
-              <p className="">Try it by clicking, tapping, or using your keyboard:</p>
-              <div style={{ color: '#777' }}>
-                <MdArrowDownward size={32} />
-              </div>
-            </div>
-            <div className="mt-4">
-              <DimensionsProvider>
-                {({ containerWidth }) => (
-                  <Piano
-                    noteRange={this.state.config.noteRange}
-                    keyboardShortcuts={keyboardShortcuts}
-                    playNote={playNote}
-                    stopNote={stopNote}
-                    disabled={isLoading}
-                    width={containerWidth}
-                  />
-                )}
-              </DimensionsProvider>
-            </div>
-            <div className="row mt-5">
-              <div className="col-lg-8 offset-lg-2">
-                <InstrumentListProvider
-                  hostname={this.props.soundfontHostname}
-                  render={(instrumentList) => (
-                    <PianoConfig
-                      config={this.state.config}
-                      setConfig={(config) => {
-                        this.setState({
-                          config: Object.assign({}, this.state.config, config),
-                        });
-                        stopAllNotes();
-                      }}
-                      instrumentList={instrumentList || [this.state.config.instrumentName]}
-                      keyboardShortcuts={keyboardShortcuts}
-                    />
-                  )}
-                />
-              </div>
+  return (
+    <SoundfontProvider
+      audioContext={audioContext}
+      instrumentName={instrumentName}
+      hostname={soundfontHostname}
+      render={({ isLoading, playNote, stopNote, stopAllNotes }) => (
+        <div>
+          <div className="text-center">
+            <p className="">Try it by clicking, tapping, or using your keyboard:</p>
+            <div style={{ color: '#777' }}>
+              <MdArrowDownward size={32} />
             </div>
           </div>
-        )}
-      />
-    );
-  }
+          <div className="mt-4">
+            <DimensionsProvider>
+              {({ containerWidth }) => (
+                <Piano
+                  noteRange={noteRange}
+                  keyboardShortcuts={keyboardShortcuts}
+                  playNote={playNote}
+                  stopNote={stopNote}
+                  disabled={isLoading}
+                  width={containerWidth}
+                />
+              )}
+            </DimensionsProvider>
+          </div>
+          <div className="row mt-5">
+            <div className="col-lg-8 offset-lg-2">
+              <InstrumentListProvider
+                hostname={soundfontHostname}
+                render={(instrumentList) => (
+                  <PianoConfig
+                    config={{ noteRange, instrumentName, keyboardShortcutOffset }}
+                    setConfig={({ noteRange, instrumentName, keyboardShortcutOffset }) => {
+                      if (instrumentName) setInstrumentName(instrumentName);
+                      if (noteRange) setNoteRange(noteRange);
+                      if (keyboardShortcutOffset) setKeyboardShortcutOffset(keyboardShortcutOffset);
+                      stopAllNotes();
+                    }}
+                    instrumentList={instrumentList || [instrumentName]}
+                    keyboardShortcuts={keyboardShortcuts}
+                  />
+                )}
+              />
+            </div>
+          </div>
+        </div>
+      )}
+    />
+  );
 }
 
 export default InteractiveDemo;
